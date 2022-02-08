ee9e4f8af5210073dec9ef8f26958e40dc66cd89
Hugo Nascimento <hugons@dcc.ufrj.br>
2022-02-08 17:21:43 -0300

    mensagem padrão adicionada

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,3 @@
-import dotenv
 import telebot
 import os
 from dotenv import load_dotenv
@@ -7,6 +6,28 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 dcc_bot = telebot.TeleBot(API_KEY)
+
+
+# Função que faz verificação um tipo de mensagem. Por enquanto, retorna true, isso faz com que o bot responda qualquer mensagem digitada no chat.
+def verificar():
+    return True
+
+# Função que mostra uma mensagem padrão no chat não importa o que o usuário digite.
+@dcc_bot.message_handler(func= True)
+def mensagem_padrao(message):
+    dcc_bot.send_message(message.chat.id,"""
+    Olá! Bem-vindo ao repositório de provas do DCC. Escolha alguma das opções abaixo de acordo com o período que deseja consultar as provas.
+
+/PrimeiroPeriodo
+/SegundoPeriodo
+/TerceiroPeriodo
+/QuartoPeriodo
+/QuintoPeriodo
+/SextoPeriodo
+/SetimoPeriodo
+/OitavoPeriodo
+""")
+
 
 def main():
     dcc_bot.polling()
